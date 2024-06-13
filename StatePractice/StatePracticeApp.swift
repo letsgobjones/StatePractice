e34ad0cc164b2e278e7d0daa981ab13d9db881ac
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-06-13 17:01:05 +0900

    Initial Commit

---
+++ StatePractice/StatePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatePracticeApp.swift
+//  StatePractice
+//
+//  Created by Brandon Jones on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct StatePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
